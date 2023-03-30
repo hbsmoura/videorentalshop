1f99e9b8bc809a0af6ca9eec1a485dab5cfdf618
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-03-30 14:58:32 -0300

    Changes Swagger title and description

--- src/main/java/com/hbsmoura/videorentalshop/VideoRentalShopApplication.java
+++ src/main/java/com/hbsmoura/videorentalshop/VideoRentalShopApplication.java
@@ -1,9 +1,18 @@
 package com.hbsmoura.videorentalshop;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info=@Info(
+				title="Video Rental Shop API",
+				description = "API for managing a video rental shop, with access control for clients " +
+						"and employees, with hierarchy levels for employees (regular and manager)."
+		)
+)
 public class VideoRentalShopApplication {
 
 	public static void main(String[] args) {
