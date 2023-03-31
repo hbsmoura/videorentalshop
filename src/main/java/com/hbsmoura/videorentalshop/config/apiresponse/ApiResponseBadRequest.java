32834d745ef7fe7a4087793a1527e32c4a6f252b
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-03-31 14:18:03 -0300

    Adds API definitions at method level on MovieController class

---
+++ src/main/java/com/hbsmoura/videorentalshop/config/apiresponse/ApiResponseBadRequest.java
@@ -0,0 +1,39 @@
+package com.hbsmoura.videorentalshop.config.apiresponse;
+
+import io.swagger.v3.oas.annotations.extensions.Extension;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.links.Link;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@ApiResponse(
+        responseCode = "400",
+        description = "BAD REQUEST"
+)
+public @interface ApiResponseBadRequest {
+    @AliasFor(annotation = ApiResponse.class)
+    Header[] headers() default {};
+
+    @AliasFor(annotation = ApiResponse.class)
+    Link[] links() default {};
+
+    @AliasFor(annotation = ApiResponse.class)
+    Content[] content() default {};
+
+    @AliasFor(annotation = ApiResponse.class)
+    Extension[] extensions() default {};
+
+    @AliasFor(annotation = ApiResponse.class)
+    String ref() default "";
+
+    @AliasFor(annotation = ApiResponse.class)
+    boolean useReturnTypeSchema() default false;
+}
--- src/main/java/com/hbsmoura/videorentalshop/controller/MovieController.java
+++ src/main/java/com/hbsmoura/videorentalshop/controller/MovieController.java
@@ -1,16 +1,24 @@
 package com.hbsmoura.videorentalshop.controller;
 
+import com.hbsmoura.videorentalshop.config.apiresponse.ApiResponseNotFound;
+import com.hbsmoura.videorentalshop.config.apiresponse.ApiResponseOk;
+import com.hbsmoura.videorentalshop.config.exceptionhandling.ApiErrorResponse;
 import com.hbsmoura.videorentalshop.dtos.MovieDto;
 import com.hbsmoura.videorentalshop.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,6 +29,12 @@ public class MovieController {
 
     private final MovieService movieService;
 
+    private static class PageOfMovieDto extends PageImpl<MovieDto> {
+        public PageOfMovieDto(List<MovieDto> content) {
+            super(content);
+        }
+    }
+
     @Autowired
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
@@ -29,43 +43,82 @@ public class MovieController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('EMPLOYEE')")
+    @Operation(
+            summary = "Create movie",
+            description = "Creates a new movie, saves on database and retrieves it"
+    )
     public MovieDto createMovie(@RequestBody @Valid MovieDto givenMovie) {
         return movieService.createMovie(givenMovie);
     }
 
     @GetMapping
+    @Operation(
+            summary = "List movies",
+            description = "Retrieves a paged list of movies"
+    )
     public Page<MovieDto> listMovies(Pageable pageable) {
         return movieService.listMovies(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Get Movie By Id",
+            description = "Retrieves a movie by its id"
+    )
+    @ApiResponseOk(content = @Content(schema = @Schema(implementation = MovieDto.class)))
+    @ApiResponseNotFound(content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     public MovieDto getMovieById(@PathVariable UUID id) {
         return movieService.getMovieById(id);
     }
 
     @GetMapping("/search/{text}")
+    @Operation(
+            summary = "Search movies by title, direction, or info",
+            description = "Retrieves a list of movies according to the given parameter text"
+    )
     public Page<MovieDto> searchMoviesByTitleDirectionOrInfo(@PathVariable String text, Pageable pageable) {
         return movieService.searchMoviesByTitleOrDirectionOrInfo(text, pageable);
     }
 
     @GetMapping("/search/genre/{genre}")
+    @Operation(
+            summary = "Search movies by genre",
+            description = "Retrieves a list of movies according to the given genre"
+    )
+    @ApiResponseOk(content = @Content(schema = @Schema(implementation = PageOfMovieDto.class)))
+    @ApiResponseNotFound(content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     public Page<MovieDto> searchMoviesByGenre(@PathVariable String genre, Pageable pageable) {
         return movieService.searchMoviesByGenre(genre, pageable);
     }
 
     @GetMapping("/search/theme/{theme}")
+    @Operation(
+            summary = "Search movies by theme",
+            description = "Retrieves a list of movies according to the given theme"
+    )
     public Page<MovieDto> searchMoviesByTheme(@PathVariable String theme, Pageable pageable) {
         return movieService.searchMoviesByTheme(theme, pageable);
     }
 
     @PutMapping
     @PreAuthorize("hasRole('EMPLOYEE')")
+    @Operation(
+            summary = "Update movie",
+            description = "Updates the movie data"
+    )
+    @ApiResponseOk(content = @Content(schema = @Schema(implementation = MovieDto.class)))
+    @ApiResponseNotFound(content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     public MovieDto updateMovie(@RequestBody @Valid MovieDto givenMovie) {
         return movieService.updateMovie(givenMovie);
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('MANAGER')")
+    @Operation(
+            summary = "Delete movie",
+            description = "Deletes a movie from database"
+    )
+    @ApiResponseNotFound(content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     @ResponseStatus(value = HttpStatus.NO_CONTENT, reason = "Movie with given Id successfully deleted")
     public void deleteMovie(@PathVariable UUID id) {
         movieService.deleteMovieById(id);
