c2ee959f0f378476362908defea0ca136cc0eb51
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-06-04 23:27:04 -0300

    Add aspect configuration

---
+++ src/main/java/com/hbsmoura/videorentalshop/config/aspects/AspectConfig.java
@@ -0,0 +1,10 @@
+package com.hbsmoura.videorentalshop.config.aspects;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.hbsmoura.videorentalshop")
+public class AspectConfig { }
